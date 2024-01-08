1ce0d5e0055c0bce2df87431d632e794684ab93a
Saurabh Netake <154451751+SaurabhNetake3303@users.noreply.github.com>
2024-01-08 17:16:53 +0530

    Update apps.js

--- apps.js
+++ apps.js
@@ -5,18 +5,18 @@ const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector("#msg");
 
 const userScorePara = document.querySelector("#user-score");
-const compScorePara = document.querySelector("#comp-score")
+const compScorePara = document.querySelector("#comp-score");
 
 const genCompChoice = () => {
     const options = ["rock" , "paper" , "scissors"];
     const randIdx = Math.floor(Math.random() * 3);
     return options[randIdx];
-}
+};
 
 const drawGame = () => {
     msg.innerText = "Game was Draw!. Play Agian.";
     msg.style.backgroundColor = "#081b31";
-}
+};
 
 const showWinner = (userWin, userChoice , compChoice) => {
     if(userWin) {
@@ -30,7 +30,7 @@ const showWinner = (userWin, userChoice , compChoice) => {
         msg.innerText = msg.innerText = `You Lose! ${compChoice} beats your ${userChoice}`;
         msg.style.backgroundColor = "red";
     }
-}
+};
 
 const playGame = (userChoice) => {
     console.log("user choice =" , userChoice);
